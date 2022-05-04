967445eec78e27c3e387bcb1f0609b8b2e30abd7
clwest <chris@donkeybetz.com>
2022-05-03 20:20:33 -0600

    added +/- DI

--- alert.py
+++ alert.py
@@ -32,9 +32,9 @@ WEBHOOK_URL = "https://discord.com/api/webhooks/971160938306756618/TrBzjftWuHzLD
 
 if last_row['ADX_14'] >= 25:
     if last_row['DMP_14'] > last_row['DMN_14']:
-        message = f"STRONG UPTREND: The ADX is {last_row['ADX_14']:.2f}"
+        message = f"STRONG UPTREND: The ADX is {last_row['ADX_14']:.2f} +DI {last_row['DMP_14']:.2f} -DI {last_row['DMN_14']:.2f}."
     if last_row['DMN_14'] > last_row['DMP_14']:
-        message = f"STRONG DOWNTREND: The ADX is {last_row['ADX_14']:.2f}"
+        message = f"STRONG DOWNTREND: The ADX is {last_row['ADX_14']:.2f} +DI {last_row['DMP_14']:.2f} -DI {last_row['DMN_14']:.2f}."
 
     payload = {
         "username": "Donkey Bot",
@@ -44,7 +44,7 @@ if last_row['ADX_14'] >= 25:
     requests.post(WEBHOOK_URL, json=payload)
 
 if last_row['ADX_14'] < 25:
-    message = f"NO TREND: The ADX is {last_row['ADX_14']:.2f}"
+    message = f"NO TREND: The ADX is {last_row['ADX_14']:.2f} +DI {last_row['DMP_14']:.2f} -DI {last_row['DMN_14']:.2f}."
 
     payload = {
         "username": "DonkeyBot",
